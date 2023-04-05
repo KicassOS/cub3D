26a73d6b6f93aa18aede1c8cc4d045e9993d82cd
gabriel <gaabrielkeller19@gmail.com>
2023-04-05 12:38:23 +0200

    The map file must not end with a newline

--- src/parsing/get_file_utils_2.c
+++ src/parsing/get_file_utils_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_file_utils_2.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gkitoko <gkitoko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 00:21:43 by pszleper          #+#    #+#             */
-/*   Updated: 2023/04/04 00:22:00 by pszleper         ###   ########.fr       */
+/*   Updated: 2023/04/05 12:35:10 by gkitoko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 int	file_to_array(t_data *data, char *file_store)
 {
+	int	i;
+
+	i = 0;
 	if (!(file_store))
 		return (INVALID_VALUE);
 	data->file_arr = ft_split(data, file_store, '\n');
+	while (file_store[i])
+		i++;
+	if (file_store[i - 1] == '\n')
+		error_handler(data, MAP_ERROR);
 	if (!data->file_arr)
 		return (MALLOC_ERROR);
 	return (SUCCESS);
